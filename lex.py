d826ccd25f70165f3440567ad98f26e3ff3948bf
Amit Timalsina <amittimalsina14@gmail.com>
2020-10-09 06:18:07 +0545

    Update lex.py

--- lex.py
+++ lex.py
@@ -7,7 +7,7 @@ logger.setLevel(logging.DEBUG)
 def lambda_handler(event, context):
     logger.debug(event)
     timezone = event['currentIntent']['slots']['Timezone']
-    weather = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Butwal, Nepal&APPID=6f5035581541af152fef5536c87468a2')
+    weather = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' +timezone+ '&APPID=6f5035581541af152fef5536c87468a2')
     weather_dict = json.loads(weather.text)
     data = weather_dict['weather'][0]['description']
     # text = 'The weather is - ' + data + '.'
